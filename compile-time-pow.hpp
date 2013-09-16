95ddd314fefce80f50f920e561b236fb584b3810
Gao Xiang <qasdfgtyuiop@gmail.com>
2013-09-16 21:21:20 +0800

    add compile time power

---
+++ compile-time-pow.hpp
@@ -0,0 +1,19 @@
+#ifndef COMPILE_TIME_POW_HPP
+#define COMPILE_TIME_POW_HPP
+
+namespace cpp_calc{
+
+template <typename T,T base,int index>
+class compile_time_pow{
+public:
+	static constexpr T value = base*compile_time_pow<T,base,index-1>::value;
+};
+
+template <typename T,T base>
+class compile_time_pow<T,base,0>{
+public:
+	static constexpr T value = T(1);
+};
+}
+
+#endif
